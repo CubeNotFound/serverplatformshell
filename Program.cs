6d3280eccb67e94133745634d185b5e533f6da4c
CubeNotFound <cubenotfound@outlook.sg>
2025-12-30 10:30:48 +0800

    Added base VS project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace serverplatformshell
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
