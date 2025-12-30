ceec98ef9c0a0f9c32cab30eef4ef60093f0b0bd
CubeNotFound <cubenotfound@outlook.sg>
2025-12-30 14:29:01 +0800

    Basic login

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,38 @@ namespace serverplatformshell
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.Write("> ");
+                string input = Console.ReadLine();
+
+                if (input.StartsWith("LOGIN", StringComparison.OrdinalIgnoreCase))
+                {
+                    string[] split = input.Split(char.Parse(" "));
+                    var url = "http://localhost:5678/auth/login";
+                    var json = $"{{\"username\":\"{split[1]}\",\"password\":\"{split[2]}\"}}";
+
+                    var request = (HttpWebRequest)WebRequest.Create(url);
+                    request.Method = "POST";
+                    request.ContentType = "application/json";
+
+                    byte[] data = Encoding.UTF8.GetBytes(json);
+                    request.ContentLength = data.Length;
+
+                    using (var stream = request.GetRequestStream())
+                    {
+                        stream.Write(data, 0, data.Length);
+                    }
+
+                    using (var response = (HttpWebResponse)request.GetResponse())
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string responseBody = reader.ReadToEnd();
+                        Console.WriteLine("Status: " + response.StatusCode);
+                        Console.WriteLine(responseBody);
+                    }
+                }
+            }
         }
     }
 }
